8b955365ee0bb44078c4c80a6fd078da9363b958
Keith Kirkpatrick <keithkirkpatrick@Keiths-MacBook-Pro.local>
2019-03-03 23:18:44 -0800

    Cleaning notebooks and flask app.

--- Flask/app.py
+++ Flask/app.py
@@ -17,194 +17,145 @@ from nltk.stem import WordNetLemmatizer, SnowballStemmer
 from nltk.stem.porter import *
 from gensim.corpora import Dictionary
 
+app = flask.Flask(__name__)
 
-df = pd.read_pickle('../Data/01_sf_recommender_pca')
+############################################################################
+########################## Load necessary pickles ##########################
+############################################################################
+
+df = pd.read_pickle('../Data/01_sf_recommender_pca_b')
 df.reset_index(inplace=True, drop=True)
 
-def get_input():
-	return input()
+pickle_in = open('Tools_and_models/top_ngrams','rb')
+top_ngrams = pickle.load(pickle_in)
+pickle_in.close()
+
+pickle_in = open('Tools_and_models/tf_idf_vectorizer','rb')
+tf_idf_vectorizer = pickle.load(pickle_in, errors='loose')
+pickle_in.close()
+
+pickle_in = open('Tools_and_models/tf_idf_model','rb')
+tf_idf = pickle.load(pickle_in, errors='loose')
+pickle_in.close()
+
+pickle_in = open('Tools_and_models/pca_tool','rb')
+pca = pickle.load(pickle_in, errors='loose')
+pickle_in.close()
 
 def format_text(text):
+    """Reads a single string. Removes punctuation and makes text lowercase. Removes stopwords. Returns as a list."""
 
-	text = re.sub('[\W_]+', ' ', text)
-	text = re.sub('[%s]' % re.escape(string.punctuation), ' ', text.lower())
+    text = re.sub('[\W_]+', ' ', text)
+    text = re.sub('[%s]' % re.escape(string.punctuation), ' ', text.lower())
 
-	stop = set(stopwords.words('english'))
-	text = [item for item in text.split() if item not in stop]
+    stop = set(stopwords.words('english'))
+    text = [item for item in text.split() if item not in stop]
 
-	lem = WordNetLemmatizer()
-	text = [lem.lemmatize(token) for token in text]
+    lem = WordNetLemmatizer()
+    text = [lem.lemmatize(token) for token in text]
 
-	stop2 = ['data', 'code', 'team', 'work']
-	text = [item for item in text if item not in stop2]
+    stop2 = ['data', 'code', 'team', 'work']
+    text = [item for item in text if item not in stop2]
 
-	return text
+    return text
 
+def create_ngrams(text, top_ngrams):
+    """Takes in formmatted text and top_ngrams as lists.
+    Creates ngrams and checks whether any created ngrams match those in created when looking at the documentself.
+    Returns text with matching ngrams as a string."""
 
+    doc_ngrams = []
 
-def create_ngrams(text):
+    for i, j in zip(text, text[1:]):
+        bigram = '_'.join([i, j])
+        doc_ngrams.append(bigram)
 
-	doc_ngrams = []
+    for i, j, k in zip(text, text[1:], text[2:]):
+        trigram = '_'.join([i, j, k])
+        doc_ngrams.append(trigram)
 
-	for i, j in zip(text, text[1:]):
-		bigram = '_'.join([i, j])
-		doc_ngrams.append(bigram)
-
-	for i, j, k in zip(text, text[1:], text[2:]):
-		trigram = '_'.join([i, j, k])
-		doc_ngrams.append(trigram)
-
-	for i, j, k, l in zip(text, text[1:],
+    for i, j, k, l in zip(text, text[1:],
 						  text[2:], text[3:]):
-		quadgram = '_'.join([i, j, k, l])
-		doc_ngrams.append(quadgram)
+        quadgram = '_'.join([i, j, k, l])
+        doc_ngrams.append(quadgram)
 
-	pickle_in = open('Tools_and_models/top_ngrams','rb')
-	top_ngrams = pickle.load(pickle_in, errors='loose')
-	pickle_in.close()
+    top_ngrams = top_ngrams.split()
+    for doc_ngram in doc_ngrams:
+        for top_ngram in top_ngrams:
+            if doc_ngram == top_ngram:
+                text.append(doc_ngram)
+            else:
+                pass
 
-	top_ngrams = top_ngrams.split()
+    return ' '.join(text)
 
-	for doc_ngram in doc_ngrams:
-		for top_ngram in top_ngrams:
-			if doc_ngram == top_ngram:
-				text.append(doc_ngram)
-			else:
-				pass
-	return ' '.join(text)
+def vectorize(text, tf_idf_vectorizer, tf_idf):
+    """Takes in ngramed text as a string, tf_idf_vectorizer, and tf_idfself.
+    Vectorizes text and uses tf_idf for feature names in a DataFrame."""
 
+    text_vector_array = tf_idf_vectorizer.transform([text]).toarray()
+    text_vector = pd.DataFrame(text_vector_array,
+    				columns=tf_idf.get_feature_names())
 
-def vectorize(text):
-	pickle_in = open('Tools_and_models/tf_idf_vectorizer','rb')
-	tf_idf_vectorizer = pickle.load(pickle_in, errors='loose')
-	pickle_in.close()
+    return text_vector
 
-	pickle_in = open('Tools_and_models/tf_idf_model','rb')
-	tf_idf = pickle.load(pickle_in, errors='loose')
-	pickle_in.close()
+def pcaitize(text_vector, pca):
+    """Performs a principal compnenent analysis on the newly created text vector and returns it as a DataFrame."""
 
-	text_vector_array = tf_idf_vectorizer.transform([text]).toarray()
-
-	text_vector = pd.DataFrame(text_vector_array,
-					columns=tf_idf.get_feature_names())
-
-	return text_vector
-
-
-def pcaitize(text_vector):
-
-	pickle_in = open('Tools_and_models/pca_tool','rb')
-	pca = pickle.load(pickle_in, errors='loose')
-	pickle_in.close()
-
-	text_pca = pca.transform(text_vector)
-
-	return pd.DataFrame(text_pca)
+    text_pca = pca.transform(text_vector)
+    return pd.DataFrame(text_pca)
 
 def recommender(df, text_pca,n_recommendations=5):
 	# find cosine similarity
-	recos = (pd.DataFrame(df.iloc[:,4:].T
+    recos = (pd.DataFrame(df.iloc[:,4:].T
 			.apply(lambda x: np.dot(x,text_pca.iloc[0,:]))))
-	largest_indeces = recos[0].nlargest(n_recommendations).index
-	recommendations = df.iloc[largest_indeces].posting_url
-	return recommendations
-
-
-app = flask.Flask(__name__)
-#
-# @app.route("/")
-# def viz_page():
-# 	with open("templates/job_recommender.html", 'r') as viz_file:
-# 		return viz_file.read()
-
-# @app.route("/", methods=["POST", "GET"])
-# def answer():
-# 	def make_recommendations(text, df, n_recommendations=5):
-# 		formatted_text = format_text(text)
-# 		ngramed_text = create_ngrams(formatted_text)
-# 		text_vector = vectorize(ngramed_text)
-# 		text_pca = pcaitize(text_vector)
-# 		recommendations = recommender(df, text_pca,n_recommendations=5)
-# 		return recommendations.values
-#
-# 	data = flask.request.json
-# 	test_case_list = data["question"]
-# 	tmp = test_case_list[0]
-#
-# 	answer = list(make_recommendations(tmp,df))
-#
-# 	print(answer)
-#
-#
-#
-# 	# return flask.jsonify({'answer':answer})
-# 	return flask.render_template('job_recommender.html',recotest=answer)
+    largest_indeces = recos[0].nlargest(n_recommendations).index
+    recommendations = df.iloc[largest_indeces][['company_name','job_title','posting_url']]
+    return pd.DataFrame(recommendations)
 
 answer = ''
 
+#############################################################################
+########################### Page render functions ###########################
+#############################################################################
+
+# Link to plotly
 @app.route("/plotly", methods=["GET"])
 def plotly():
     return flask.render_template('plotly_embedded.html')
 
+# Pulls up and populates the results page
 @app.route("/results", methods=["GET"])
 def results():
-	with open ('answer.csv', 'r') as r:
-		answer = r.read()
-	r.close()
-	return flask.render_template('test.html', answer = answer)
+	answer = pd.read_pickle('answer')
+	return flask.render_template('main_page.html', answer = answer)
 
+# main_page load
 @app.route("/", methods=["POST", "GET"])
 def test():
-	def make_recommendations(text, df, n_recommendations=5):
-		formatted_text = format_text(text)
-		ngramed_text = create_ngrams(formatted_text)
-		text_vector = vectorize(ngramed_text)
-		text_pca = pcaitize(text_vector)
-		recommendations = recommender(df, text_pca,n_recommendations=5)
-		return recommendations.values
+    def make_recommendations(text, df, n_recommendations=5):
+        formatted_text = format_text(text)
+        ngramed_text = create_ngrams(formatted_text, top_ngrams)
+        text_vector = vectorize(ngramed_text, tf_idf_vectorizer, tf_idf)
+        text_pca = pcaitize(text_vector, pca)
+        recommendations = recommender(df, text_pca,n_recommendations=5)
+        return recommendations.values
 
-	data = flask.request.json
+	# Collections user input
+    data = flask.request.json
 
-	if data is None:
-		answer = ''
+	# User input will be empty upon page load. Must account for that occurence.
+    if data is None:
+        answer = pd.DataFrame()
 
-	else:
-		print(data["question"][0][:10])
-		test_case_list = data["question"]
-		tmp = test_case_list[0]
+    else:
+        tmp = data["question"][0]
 
-		answer = list(make_recommendations(tmp,df))
+        answer = make_recommendations(tmp,df)
+        answer = pd.DataFrame(answer)
+        answer.to_pickle('answer')
 
-		with open ('answer.csv', 'w') as w:
-			for line in answer:
-				w.write(line)
-				w.write(', ')
-		w.close()
-
-		return flask.redirect(flask.url_for('response'))
-
-	return flask.render_template('test.html', answer = answer)
-
-@app.route("/response", methods=["POST", "GET"])
-def response():
-	with open ('answer.csv', 'r') as r:
-		answer = r.read()
-	r.close()
-	print('answer is', answer)
-	return flask.render_template('test.html', answer = answer)
-
-@app.route("/redirectme")
-def redirectme():
-	return flask.redirect(flask.url_for('redirecthere'))
-
-@app.route("/redirecthere")
-def redirecthere():
-	with open ('answer.csv', 'r') as r:
-		answer = r.read()
-	r.close()
-	return flask.render_template('test.html', answer = answer)
-
-
+    return flask.render_template('main_page.html', answer = answer)
 
 if __name__=="__main__":
 	app.run(debug=True, threaded=True)
